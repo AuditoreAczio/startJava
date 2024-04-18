d067e308bbeffd88ca449b28f664b1e30b648fb2
AuditoreAczio <167468800+AuditoreAczio@users.noreply.github.com>
2024-04-18 21:59:21 +0300

    Add files via upload

---
+++ VariableNamesTheme.java
@@ -0,0 +1,118 @@
+public class VariableNamesTheme {
+
+    public static void main(String[] args) {
+
+    //TODO цифра
+
+        int number = 7;
+
+        System.out.println(number);
+
+    //TODO сумма простых чисел
+
+         int sum = 1 + 2;
+
+        System.out.println(sum);
+
+    //TODO произведение чисел 6 и 4
+
+       int x =6, y = 4;
+
+        int z = x * y;
+
+        System.out.println(z);
+
+    //TODO максимально число к примеру 43
+
+        int max = 43;
+
+        System.out.println(max);
+
+    //TODO количество десятков
+
+        int number_of_tens = 10;
+
+        System.out.println(number_of_tens);
+
+    //TODO вес собаки
+
+        int sizeDogWeight = 14;
+
+       System.out.println(sizeDogWeight);
+
+     //TODO исходное число
+
+        int original_number = 5;
+
+        System.out.println(original_number);
+
+     //TODO процент по вкладу
+
+        int percent = 15;
+
+        System.out.println(percent);
+
+     //TODO код ошибки
+
+        int error_code = 144;
+
+        System.out.println(error_code);
+
+     //TODO счет в игре
+
+         int game_score = 23;
+
+         System.out.println(game_score); 
+
+     //TODO максимальная длина
+
+         int max_lenght = 34;
+
+         System.out.println(max_lenght);
+
+     //TODO выбор 
+
+         int choose = 3;
+
+         System.out.println(choose);
+
+     //TODO тип сообщений
+
+         int message_type = 4;
+
+         System.out.println(message_type);
+
+     //TODO математическое выражение
+
+         int mathematical_expression = 67;
+
+         System.out.println(mathematical_expression);
+
+     //TODO случайное число
+
+         int random_number = 5;
+
+         System.out.println(random_number);
+
+     //TODO число нулей
+
+         int number_of_zeros = 6;
+
+         System.out.println(number_of_zeros);
+
+      //TODO стоимость кофе на вынос
+      
+         int cost_coffee_to_go = 154;
+
+         System.out.println(cost_coffee_to_go);
+
+      //TODO дата начала 
+
+         int start_date = 23;
+
+         System.out.println(start_date);
+
+      //TODO 
+
+    }
+}

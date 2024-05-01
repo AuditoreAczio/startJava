922d0464c8b1671236169288675dff2e3eb6ac1f
AuditoreAczio <167468800+AuditoreAczio@users.noreply.github.com>
2024-05-01 20:58:51 +0300

    Add files via upload

---
+++ VariableTheme.java
@@ -0,0 +1,32 @@
+public class VariableTheme {
+    public static void main(String[] args) {
+        System.out.println("\n1. Вывод характеристик компьютера: ");
+        
+        // характеристики видеокарты
+        byte geForce1660Ti = 6;
+        System.out.println(geForce1660Ti);
+
+        // частота процессора
+        char intelCoreI510400F = 2.90;
+        System.out.println(intelCoreI510400F);
+
+        //оперативная память
+        int ram = 32;
+        System.out.println(ram);
+
+        //общая память
+        short hddTb = 1;
+        System.out.println(hddTb);
+
+        //ssd
+        float ssd = 232;
+        System.out.println(ssd);
+
+        // ядра процессора
+        long processorCores = 6;
+        System.out.println(processorCores);
+
+        //блок питания
+        double powerUnit = 600;
+        System.out.println(powerUnit);
+

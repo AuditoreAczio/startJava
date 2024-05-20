b4bd90829bb38ea8e6147748513ff14d5c19e3ee
AuditoreAczio <167468800+AuditoreAczio@users.noreply.github.com>
2024-05-20 12:22:12 +0300

    Add files via upload

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -3,35 +3,30 @@ public class VariablesTheme {
         System.out.println("\n1. Вывод характеристик компьютера: ");
  
         byte vramAmount = 6;
-        System.out.println(vramAmount);
-
         double cpuFrequency = 2.90;
-        System.out.println(cpuFrequency);
-
         long ramSize = 32;
-        System.out.println(ramSize);
-
         short hddSize = 1;
-        System.out.println(hddSize);
-
-        int ssdSize = 232;
-        System.out.println(ssdSize);
-
+        int ssdSize = 232;;
         float cpuCores = 6;
-        System.out.println(cpuCores);
-
-        boolean isPcOn = true;
-        System.out.println(isPcOn);
+        boolean isPcOn = true; System.out.println("\nВидеопамять: " + vramAmount + 
+            "\nЧастота процессора: " + cpuFrequency + 
+            "\nОбъём оперативки: " + ramSize + 
+            "\nПамять жесткого диска: " + hddSize + 
+            "\nПамять ssd: " + ssdSize + 
+            "\nядра процессора" + cpuCores + 
+            "\nпк включен: ");
 
         System.out.println("\n2. Расчет стоимости товара со скидкой: ");
 
         double priceBook = 235.83;
         double pricePen = 105.5;
-        int discount = 11;
+        double discount = 0.11;
         double sum = priceBook + pricePen;
-        double totalDis = pricePen / 100 * discount + priceBook / 100 * discount;
-        double sumDiscount = sum - totalDis;
-        System.out.println("sum: " + sum + ", totalDis: " + totalDis + ", sumDiscount: " + sumDiscount);
+        double totalDiscount = pricePen / 100 * discount + priceBook / 100 * discount;
+        double sumDiscount = sum - totalDiscount;
+        System.out.println("\nобщую стоимость товаров без скидки: " + sum + 
+                "\nсумму скидки: " + totalDiscount + 
+                "\nобщую стоимость товаров со скидкой: " + sumDiscount);
 
         System.out.println("\n\n3. Вывод слова Java\n");
         System.out.println("   J    a  v     v  a     ");
@@ -45,18 +40,18 @@ public class VariablesTheme {
         short shortMax = 32_767;
         int intMax = 2_147_483_647;
         long longMax = 9_223_372_036_854_775_807L;
-        System.out.println("Исходное значение byte: " + byteMax +
-        "\nПосле инкремента: " + ++byteMax +
-        "\nПосле декремента: " + --byteMax +
-        "Исходное значение short: " + shortMax +
-        "\nПосле инкремента: " + ++shortMax +
-        "\nПосле декремента: " + --shortMax +
-        "Исходное значение int: " + intMax +
-        "\nПосле инкремента: " + ++intMax +
-        "\nПосле декремента: " + --intMax +
-        "Исходное значение: long" + longMax +
-        "\nПосле инкремента: " + ++longMax +
-        "\nПосле декремента: " + --longMax);
+        System.out.println("\nИсходное значение byte: " + byteMax + 
+                "\nПосле инкремента: " + ++byteMax + 
+                "\nПосле декремента: " + --byteMax + 
+                "\nИсходное значение short: " + shortMax + 
+                "\nПосле инкремента: " + ++shortMax + 
+                "\nПосле декремента: " + --shortMax + 
+                "\nИсходное значение int: " + intMax + 
+                "\nПосле инкремента: " + ++intMax + 
+                "\nПосле декремента: " + --intMax + 
+                "\nИсходное значение: long: " + longMax + 
+                "\nПосле инкремента: " + ++longMax + 
+                "\nПосле декремента: " + --longMax);
 
         System.out.println("\n5. Перестановка значений переменных");
         int a = 2;
@@ -95,20 +90,24 @@ public class VariablesTheme {
 
 
         System.out.println("\n8 Вывод количества сотен, десятков и единиц числа");
-        int n = 123;
-        int x = (n / 100) % 10;
-        int y = (n / 10) % 10;
-        int z = (n / 1) % 10;
-        int s = z + y + x;
-        int p = z * y * x;
-        System.out.println("z: " + z + ", y: " + y + ", x: " + x + ", s: " + s + ", p: " + p);
+        int number = 123;
+        int hundreds = (number / 100) % 10;
+        int dozens = (number / 10) % 10;
+        int units = (number / 1) % 10;
+        int summa = hundreds + dozens + units;
+        int composition = hundreds * dozens * units;
+        System.out.println("hundreds: " + hundreds + 
+                ", dozens: " + dozens + 
+                ", units: " + units + 
+                ", summa: " + summa + 
+                ", composition: " + composition);
 
         System.out.println("\n9 Вывод времени");
         int totalSec = 86399;
         int hh = (totalSec / 3600);
         int mm = (totalSec % 3600) / 60;
         int ss = totalSec % 60;
-        System.out.println("hh: " + hh + ", mm: " + mm + ", ss: " + ss);
+        System.out.println(hh + ":" + mm + ":" + ss);
     }
 }
 
